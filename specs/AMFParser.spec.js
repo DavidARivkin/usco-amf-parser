e449f4f1b12933a94d0d40d35c765ee707769c79
Mark Moissette <kaosat.dev@gmail.com>
2013-12-05 23:14:34 +0100

    - fixed missing dependencies in package.json - cleaned up / fixed jasmine node side test : all ok, for compressed and uncompressed amf files  * basic tests only for now

--- specs/AMFParser.spec.js
+++ specs/AMFParser.spec.js
@@ -1,13 +1,24 @@
-AMFParser = require("../AMFParser")
-THREE = require("THREE")
+AMFParser = require("../AMFParser");
+THREE = require("three");
+fs = require("fs");
 
 describe("AMF parser tests", function() {
   var parser = new AMFParser();
 
-  it("can parse uncompressed amf files", function() {
-
-    data = "";
-    parser.parse(data);
-    expect(data).toBe(true);
+  it("can parse compressed amf files", function() {
+    data = fs.readFileSync("specs/data/stapel_dual.amf",'binary')
+    parsedAmf = parser.parse(data);
+    expect(parsedAmf instanceof THREE.Object3D).toBe(true);
+    expect(parsedAmf.children[0].geometry.vertices.length).toEqual(564);
   });
+
+  it("can parse uncompressed amf files", function() {
+    data = fs.readFileSync("specs/data/Rook.amf",'binary')
+    parsedAmf = parser.parse(data);
+    expect(parsedAmf instanceof THREE.Object3D).toBe(true);
+    expect(parsedAmf.children[0].name).toEqual("Default");
+    expect(parsedAmf.children[0].geometry.vertices.length).toEqual(1843);
+  });
+
+
 });
